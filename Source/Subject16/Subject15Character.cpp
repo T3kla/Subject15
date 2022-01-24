45ca65190bedbef1b33ab5d504f0206630300ea3
T3kla <manuillo94@gmail.com>
2022-01-24 21:01:16 +0100

    Character full Cpp

--- Source/Subject16/Subject15Character.cpp
+++ Source/Subject16/Subject15Character.cpp
@@ -1,52 +1,102 @@
 #include "Subject15Character.h"
 
-ASubject15Character::ASubject15Character() {
-  PrimaryActorTick.bCanEverTick = true;
+ASubject15Character::ASubject15Character()
+{
+    PrimaryActorTick.bCanEverTick = true;
 
-  auto *Capsule = GetCapsuleComponent();
-  SetRootComponent(Capsule);
+    auto *Capsule = GetCapsuleComponent();
+    SetRootComponent(Capsule);
 
-  SpringArmCompCpp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmCompCpp"));
-  SpringArmCompCpp->SetupAttachment(Capsule);
-  SpringArmCompCpp->bUsePawnControlRotation = true;
+    SpringArmCompCpp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmCompCpp"));
+    SpringArmCompCpp->SetupAttachment(Capsule);
+    SpringArmCompCpp->bUsePawnControlRotation = true;
 
-  PistolCompCpp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PistolCompCpp"));
-  PistolCompCpp->SetupAttachment(SpringArmCompCpp, USpringArmComponent::SocketName);
+    PistolCompCpp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PistolCompCpp"));
+    PistolCompCpp->SetupAttachment(SpringArmCompCpp, USpringArmComponent::SocketName);
 
-  CameraCompCpp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraCompCpp"));
-  CameraCompCpp->SetupAttachment(SpringArmCompCpp, USpringArmComponent::SocketName);
+    CameraCompCpp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraCompCpp"));
+    CameraCompCpp->SetupAttachment(SpringArmCompCpp, USpringArmComponent::SocketName);
 
-  ArrowCompCpp = CreateDefaultSubobject<UArrowComponent>(TEXT("ArrowCompCpp"));
-  ArrowCompCpp->SetupAttachment(PistolCompCpp);
+    ArrowCompCpp = CreateDefaultSubobject<UArrowComponent>(TEXT("ArrowCompCpp"));
+    ArrowCompCpp->SetupAttachment(PistolCompCpp);
 }
 
-void ASubject15Character::BeginPlay() {
-  Super::BeginPlay();
-  InputComponent->BindAxis("Forward", this, &ASubject15Character::MoveVertical);
-  InputComponent->BindAxis("Right", this, &ASubject15Character::MoveHorizontal);
-  InputComponent->BindAxis("LookUp", this, &ASubject15Character::Pitch);
-  InputComponent->BindAxis("Turn", this, &ASubject15Character::Yaw);
+void ASubject15Character::BeginPlay()
+{
+    Super::BeginPlay();
+
+    // Input Bindings
+    InputComponent->BindAxis("Forward", this, &ASubject15Character::MoveVertical);
+    InputComponent->BindAxis("Right", this, &ASubject15Character::MoveHorizontal);
+    InputComponent->BindAxis("LookUp", this, &ASubject15Character::Pitch);
+    InputComponent->BindAxis("Turn", this, &ASubject15Character::Yaw);
+    InputComponent->BindAction("Jump", IE_Pressed, this, &ASubject15Character::JumpStart);
+    InputComponent->BindAction("Jump", IE_Released, this, &ASubject15Character::JumpStop);
+    InputComponent->BindAction("WeaponToSlot1", IE_Pressed, this, &ASubject15Character::SlotOne);
+    InputComponent->BindAction("WeaponToSlot2", IE_Pressed, this, &ASubject15Character::SlotTwo);
+
+    if (!GunMaterialCpp)
+        return;
+
+    // Create Dyn Material
+    GunDynMaterialCpp = UMaterialInstanceDynamic::Create(GunMaterialCpp, this);
+
+    // Assign Dyn Material
+    auto Materials = PistolCompCpp->GetMaterials();
+    for (size_t i = 0; i < PistolCompCpp->GetMaterials().Num(); i++)
+        if (i > 0) // Skip first
+            PistolCompCpp->SetMaterial(i, GunDynMaterialCpp);
 }
 
-void ASubject15Character::MoveVertical(float Amount) {
-  if (Controller && Amount)
-    AddMovementInput(CameraCompCpp->GetForwardVector(), Amount);
+void ASubject15Character::MoveVertical(float Amount)
+{
+    if (Controller && Amount)
+        AddMovementInput(CameraCompCpp->GetForwardVector(), Amount);
 }
-void ASubject15Character::MoveHorizontal(float Amount) {
-  if (Controller && Amount)
-    AddMovementInput(CameraCompCpp->GetRightVector(), Amount);
-}
-
-void ASubject15Character::Pitch(float amount) {
-  AddControllerPitchInput(200.f * amount * GetWorld()->GetDeltaSeconds());
+void ASubject15Character::MoveHorizontal(float Amount)
+{
+    if (Controller && Amount)
+        AddMovementInput(CameraCompCpp->GetRightVector(), Amount);
 }
 
-void ASubject15Character::Yaw(float amount) {
-  AddControllerYawInput(200.f * amount * GetWorld()->GetDeltaSeconds());
+void ASubject15Character::Pitch(float amount)
+{
+    AddControllerPitchInput(200.f * amount * GetWorld()->GetDeltaSeconds());
 }
 
-void ASubject15Character::Tick(float DeltaTime) { Super::Tick(DeltaTime); }
+void ASubject15Character::Yaw(float amount)
+{
+    AddControllerYawInput(200.f * amount * GetWorld()->GetDeltaSeconds());
+}
 
-void ASubject15Character::SetupPlayerInputComponent(UInputComponent *PlayerInputComponent) {
-  Super::SetupPlayerInputComponent(PlayerInputComponent);
+void ASubject15Character::JumpStart()
+{
+    Jump();
+}
+
+void ASubject15Character::JumpStop()
+{
+    StopJumping();
+}
+
+void ASubject15Character::SlotOne()
+{
+    if (GunDynMaterialCpp)
+        GunDynMaterialCpp->SetVectorParameterValue("Color", {1.f, 0.f, 0.f, 0.f});
+}
+
+void ASubject15Character::SlotTwo()
+{
+    if (GunDynMaterialCpp)
+        GunDynMaterialCpp->SetVectorParameterValue("Color", {0.f, 1.f, 0.f, 0.f});
+}
+
+void ASubject15Character::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
+
+void ASubject15Character::SetupPlayerInputComponent(UInputComponent *PlayerInputComponent)
+{
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
 }
--- Source/Subject16/Subject15Character.h
+++ Source/Subject16/Subject15Character.h
@@ -1,43 +1,64 @@
 #pragma once
 
 #include "CoreMinimal.h"
+
 #include "GameFramework/Character.h"
+
 #include "Components/StaticMeshComponent.h"
 #include "Components/CapsuleComponent.h"
 #include "Components/ArrowComponent.h"
+
 #include "GameFramework/SpringArmComponent.h"
+
 #include "Camera/CameraComponent.h"
+
+#include "Materials/Material.h"
+
 #include "Subject15Character.generated.h"
 
 UCLASS()
-class SUBJECT16_API ASubject15Character : public ACharacter {
-  GENERATED_BODY()
+class SUBJECT16_API ASubject15Character : public ACharacter
+{
+    GENERATED_BODY()
 
-public:
-  ASubject15Character();
+  public:
+    ASubject15Character();
 
-  UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
-  USpringArmComponent *SpringArmCompCpp;
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+    USpringArmComponent *SpringArmCompCpp;
 
-  UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
-  UCameraComponent *CameraCompCpp;
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+    UCameraComponent *CameraCompCpp;
 
-  UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
-  UStaticMeshComponent *PistolCompCpp;
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+    UStaticMeshComponent *PistolCompCpp;
 
-  UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
-  UArrowComponent *ArrowCompCpp;
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+    UArrowComponent *ArrowCompCpp;
 
-protected:
-  virtual void BeginPlay() override;
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+    UMaterial *GunMaterialCpp;
 
-  virtual void MoveVertical(float Amount);
-  virtual void MoveHorizontal(float Amount);
-  virtual void Pitch(float Amount);
-  virtual void Yaw(float Amount);
+  private:
+    UMaterialInstanceDynamic *GunDynMaterialCpp;
 
-public:
-  virtual void Tick(float DeltaTime) override;
+  protected:
+    virtual void BeginPlay() override;
 
-  virtual void SetupPlayerInputComponent(class UInputComponent *PlayerInputComponent) override;
+    // Axes
+    virtual void MoveVertical(float Amount);
+    virtual void MoveHorizontal(float Amount);
+    virtual void Pitch(float Amount);
+    virtual void Yaw(float Amount);
+
+    // Actions
+    virtual void JumpStart();
+    virtual void JumpStop();
+    virtual void SlotOne();
+    virtual void SlotTwo();
+
+  public:
+    virtual void Tick(float DeltaTime) override;
+
+    virtual void SetupPlayerInputComponent(class UInputComponent *PlayerInputComponent) override;
 };
