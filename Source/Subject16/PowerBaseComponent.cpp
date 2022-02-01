9ef15a84562eab6872a290cd59fbfd8deae60585
T3kla <manuillo94@gmail.com>
2022-02-02 00:26:55 +0100

    Cooldown Again

--- Source/Subject16/PowerBaseComponent.cpp
+++ Source/Subject16/PowerBaseComponent.cpp
@@ -13,36 +13,43 @@ void UPowerBaseComponent::BeginPlay()
     Super::BeginPlay();
 }
 
-void UPowerBaseComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UPowerBaseComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                        FActorComponentTickFunction *ThisTickFunction)
 {
     Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 }
 
 void UPowerBaseComponent::FirePressed()
 {
-    if (FireMode == EFireMode::Automatic)
+    if (IsInCooldown)
+        return;
+
+    switch (FireMode)
     {
+    case EFireMode::Automatic:
         GetWorld()->GetTimerManager().SetTimer(
-            *FireTimerHandle, this, &UPowerBaseComponent::ExecutePower, 1 / FireRate, true, 0);
-    }
-    else if (FireMode == EFireMode::Semiautomatic)
-    {
+            AutomaticTimer, this, &UPowerBaseComponent::ExecutePower, FireRate, true, 0.f);
+        SetCooldown();
+        break;
+    case EFireMode::Semiautomatic:
         ExecutePower();
-    }
-    else if (FireMode == EFireMode::Laser)
-    {
+        SetCooldown();
+        break;
+    case EFireMode::Laser:
         ExecutePower();
-    }
-    else
-    {
+        SetCooldown();
+        break;
+    default:
         if (GEngine)
             GEngine->AddOnScreenDebugMessage(-1, 2.5f, FColor::Red, "INVALID FireMode Selected!");
+        break;
     }
 }
 
 void UPowerBaseComponent::FireReleased()
 {
-    GetWorld()->GetTimerManager().ClearTimer(*FireTimerHandle);
+    if (FireMode == EFireMode::Automatic)
+        GetWorld()->GetTimerManager().ClearTimer(AutomaticTimer);
 }
 
 void UPowerBaseComponent::ActivatePower()
@@ -52,9 +59,22 @@ void UPowerBaseComponent::ActivatePower()
 
 void UPowerBaseComponent::DeactivatePower()
 {
-    GetWorld()->GetTimerManager().ClearTimer(*FireTimerHandle);
 }
 
 void UPowerBaseComponent::ExecutePower()
 {
+    if (FireMode == EFireMode::Automatic)
+        SetCooldown(-0.001f);
+}
+
+void UPowerBaseComponent::SetCooldown(float Modifier)
+{
+    IsInCooldown = true;
+    GetWorld()->GetTimerManager().SetTimer(CooldownTimer, this, &UPowerBaseComponent::ResetCooldown,
+                                           FireRate + Modifier, false);
+}
+
+void UPowerBaseComponent::ResetCooldown()
+{
+    IsInCooldown = false;
 }
--- Source/Subject16/PowerBaseComponent.h
+++ Source/Subject16/PowerBaseComponent.h
@@ -33,7 +33,8 @@ class SUBJECT16_API UPowerBaseComponent : public UActorComponent
 
   protected:
     virtual void BeginPlay() override;
-    virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+    virtual void TickComponent(float DeltaTime, enum ELevelTick TickType,
+                               FActorComponentTickFunction *ThisTickFunction) override;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite)
     EFireMode FireMode = EFireMode::None;
@@ -50,7 +51,12 @@ class SUBJECT16_API UPowerBaseComponent : public UActorComponent
     UPROPERTY(EditAnywhere, BlueprintReadWrite)
     AActor *PowerVFX;
 
-    FTimerHandle *FireTimerHandle = new FTimerHandle();
+    void SetCooldown(float Modifier = 0.f);
+    void ResetCooldown();
+
+    FTimerHandle AutomaticTimer;
+    FTimerHandle CooldownTimer;
+    bool IsInCooldown = false;
 
     ASubject15Character *Character;
 };
