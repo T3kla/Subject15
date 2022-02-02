c697e01b16d11bdb75a1c9f5a8aa74a08b1e5184
Calde <chipneidev@gmail.com>
2022-02-02 14:04:48 +0100

    Sound Implement en PowerBase y en ProjectileBase

--- Source/Subject16/PowerBaseComponent.cpp
+++ Source/Subject16/PowerBaseComponent.cpp
@@ -1,80 +1,98 @@
 #include "PowerBaseComponent.h"
 #include "Subject15Character.h"
+#include "Kismet/GameplayStatics.h"
 
 UPowerBaseComponent::UPowerBaseComponent()
 {
-    PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = true;
 
-    Character = Cast<ASubject15Character>(GetOwner());
+	Character = Cast<ASubject15Character>(GetOwner());
 }
 
 void UPowerBaseComponent::BeginPlay()
 {
-    Super::BeginPlay();
+	Super::BeginPlay();
 }
 
 void UPowerBaseComponent::TickComponent(float DeltaTime, ELevelTick TickType,
-                                        FActorComponentTickFunction *ThisTickFunction)
+	FActorComponentTickFunction* ThisTickFunction)
 {
-    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 }
 
 void UPowerBaseComponent::FirePressed()
 {
-    if (IsInCooldown)
-        return;
+	if (IsInCooldown)
+		return;
 
-    switch (FireMode)
-    {
-    case EFireMode::Automatic:
-        GetWorld()->GetTimerManager().SetTimer(
-            AutomaticTimer, this, &UPowerBaseComponent::ExecutePower, FireRate, true, 0.f);
-        SetCooldown();
-        break;
-    case EFireMode::Semiautomatic:
-        ExecutePower();
-        SetCooldown();
-        break;
-    case EFireMode::Laser:
-        ExecutePower();
-        SetCooldown();
-        break;
-    default:
-        if (GEngine)
-            GEngine->AddOnScreenDebugMessage(-1, 2.5f, FColor::Red, "INVALID FireMode Selected!");
-        break;
-    }
+	switch (FireMode)
+	{
+	case EFireMode::Automatic:
+		GetWorld()->GetTimerManager().SetTimer(
+			AutomaticTimer, this, &UPowerBaseComponent::ExecutePower, FireRate, true, 0.f);
+		SetCooldown();
+		break;
+	case EFireMode::Semiautomatic:
+		ExecutePower();
+		SetCooldown();
+		break;
+	case EFireMode::Laser:
+		ExecutePower();
+		SetCooldown();
+		break;
+	default:
+		if (GEngine)
+			GEngine->AddOnScreenDebugMessage(-1, 2.5f, FColor::Red, "INVALID FireMode Selected!");
+		break;
+	}
 }
 
 void UPowerBaseComponent::FireReleased()
 {
-    if (FireMode == EFireMode::Automatic)
-        GetWorld()->GetTimerManager().ClearTimer(AutomaticTimer);
+	if (FireMode == EFireMode::Automatic)
+		GetWorld()->GetTimerManager().ClearTimer(AutomaticTimer);
 }
 
 void UPowerBaseComponent::ActivatePower()
 {
-    Character->SetPistolColor(PowerColor);
+	Character->SetPistolColor(PowerColor);
 }
 
 void UPowerBaseComponent::DeactivatePower()
 {
+
 }
 
 void UPowerBaseComponent::ExecutePower()
 {
-    if (FireMode == EFireMode::Automatic)
-        SetCooldown(-0.001f);
+	if (FireMode == EFireMode::Automatic)
+	{
+		SetCooldown(-0.001f);
+	}
+
+	PlayPowerSound();
 }
 
 void UPowerBaseComponent::SetCooldown(float Modifier)
 {
-    IsInCooldown = true;
-    GetWorld()->GetTimerManager().SetTimer(CooldownTimer, this, &UPowerBaseComponent::ResetCooldown,
-                                           FireRate + Modifier, false);
+	IsInCooldown = true;
+	GetWorld()->GetTimerManager().SetTimer(CooldownTimer, this, &UPowerBaseComponent::ResetCooldown,
+		FireRate + Modifier, false);
 }
 
 void UPowerBaseComponent::ResetCooldown()
 {
-    IsInCooldown = false;
+	IsInCooldown = false;
+}
+
+void UPowerBaseComponent::PlayPowerSound()
+{
+	if (PowerSoundFX)
+	{
+
+		FVector SoundLocation = Character->GetArrowComponent()->GetComponentTransform().GetLocation();
+
+		UGameplayStatics::PlaySoundAtLocation(this, PowerSoundFX, SoundLocation /*, float VolumeMultiplier = 1.f, float PitchMultiplier = 1.f, float StartTime = 0.f, class USoundAttenuation* AttenuationSettings = nullptr, USoundConcurrency * ConcurrencySettings = nullptr*/);
+	}
+
 }
--- Source/Subject16/ProjectileBase.h
+++ Source/Subject16/ProjectileBase.h
@@ -24,6 +24,33 @@ class SUBJECT16_API AProjectileBase : public AActor
     UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
     UProjectileMovementComponent *ProjectileMovementCompCpp;
 
+protected:
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+        float Lifetime = 2.f;
+
+
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+        USoundBase* ProjectileSoundFX;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+        UParticleSystem* ProjectileExplosionVFX;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+        FVector ExplosionScale = { .6f, .6f, .6f };
+
   protected:
     virtual void BeginPlay() override;
+
+    virtual void OnOverlap(UPrimitiveComponent* OverlappedComp, AActor* Other,
+        UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
+        const FHitResult& SweepResult);
+
+
+    virtual void DestroyProjectile();
+
+    virtual void PlayProjectileExplosionSFX();
+  
+    virtual void PlayProjectileExplosionVFX();
 };
