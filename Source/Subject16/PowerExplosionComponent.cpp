bafca4ed735ed1b83de79e3ce088a66a9c3aa184
T3kla <manuillo94@gmail.com>
2022-02-03 14:11:57 +0100

    Fix Crash when LaunchGame

--- Source/Subject16/PowerExplosionComponent.cpp
+++ Source/Subject16/PowerExplosionComponent.cpp
@@ -2,53 +2,51 @@
 #include "Subject15Character.h"
 #include "Kismet/GameplayStatics.h"
 
-
 // Sets default values for this component's properties
 UPowerExplosionComponent::UPowerExplosionComponent()
 {
-	PrimaryComponentTick.bCanEverTick = true;
+    PrimaryComponentTick.bCanEverTick = true;
 }
-
 
 // Called when the game starts
 void UPowerExplosionComponent::BeginPlay()
 {
-	Super::BeginPlay();
+    Super::BeginPlay();
 }
 
 void UPowerExplosionComponent::FirePressed()
 {
-	Super::FirePressed();
+    Super::FirePressed();
 }
 
 void UPowerExplosionComponent::FireReleased()
 {
-	Super::FireReleased();
+    Super::FireReleased();
 }
 
 void UPowerExplosionComponent::ActivatePower()
 {
-	Super::ActivatePower();
+    Super::ActivatePower();
 }
 
 void UPowerExplosionComponent::DeactivatePower()
 {
-	Super::DeactivatePower();
+    Super::DeactivatePower();
 }
 
 void UPowerExplosionComponent::ExecutePower()
 {
-	Super::ExecutePower();
+    Super::ExecutePower();
 
-	FVector A, B;
-	FHitResult Res;
-	Character->GetPistolShot(A, B, Res);
+    FVector A, B;
+    FHitResult Res;
+    Character->GetPistolShot(A, B, Res);
 
-	FVector SoundLocation = Character->GetArrowComponent()->GetComponentTransform().GetLocation();
-	UGameplayStatics::PlaySoundAtLocation(this, PowerSoundFX, SoundLocation 
+    FVector SoundLocation = Character->GetActorLocation();
+    UGameplayStatics::PlaySoundAtLocation(this, PowerSoundFX, SoundLocation 
 	/*, float VolumeMultiplier = 1.f, float PitchMultiplier = 1.f, 
 	float StartTime = 0.f, class USoundAttenuation* AttenuationSettings = nullptr, 
 	USoundConcurrency * ConcurrencySettings = nullptr*/);
 
-	GetWorld()->SpawnActor<AProjectileBase>(this->ProjectilePowerType, A, (B - A).Rotation());
+    GetWorld()->SpawnActor<AProjectileBase>(this->ProjectilePowerType, A, (B - A).Rotation());
 }
